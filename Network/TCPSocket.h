2dad22f814e99d7c939dd7e124e6fca70171ac42
CrustPizza <mysterys0901@gmail.com>
2022-05-11 00:50:36 +0900

    Network - TCPSocket 기본적인 설계

--- Network/TCPSocket.h
+++ Network/TCPSocket.h
@@ -1,9 +1,9 @@
 /****************************
 *							*
-*	Socket Interface.h		*
+*	TCP Socket.h			*
 *							*
 *	Created : 2022/05/09	*
-*	Updated : 2022/05/09	*
+*	Updated : 2022/05/10	*
 *****************************/
 
 #pragma once
@@ -12,4 +12,20 @@
 class TCPSocket : public SocketInterface
 {
 
+public:
+	// Standard
+	bool	Open() override;
+	bool	Bind(SocketAddrIn& target) override;
+	bool	connect(SocketAddrIn& target) override;
+
+	void	Close() override;
+	void	Shutdown(int option = SD_BOTH) override;
+
+	// Input / Output
+	int		Recv(char* buffer, int size, int& error);
+	int		Send(char* buffer, int size, int& error);
+
+	// Listen
+	bool	Listen();
+	SOCKET	Accept(SocketAddrIn& target);
 };
